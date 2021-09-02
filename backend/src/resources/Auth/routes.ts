c66d45b7f8512642681a5d996449cedc1e5af18f
brokensythe <ade_1.0@live.co.uk>
2021-09-02 13:46:57 +0100

    auth and user signup

---
+++ backend/src/resources/Auth/routes.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import { loginUser } from "./controller";
+
+const authRouter = Router()
+
+authRouter.route("/login").post(loginUser)
+
+export default authRouter 
--- frontend/MIXEDUP-RECORDS/src/components/LandingPageHeader.tsx
+++ frontend/MIXEDUP-RECORDS/src/components/LandingPageHeader.tsx
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { SyntheticEvent } from "react";
 import "../components/landingPageHeaderStyles.css";
 import { useStore } from "../Hooks/Store";
+import { useHistory } from "react-router-dom";
 
 export default function LandingPageHeader() {
   const setModal = useStore((store) => store.setModal);
+  const history = useHistory()
+
+  function handlesubmit(event: SyntheticEvent) {
+    
+    const {
+      "email" : email,
+      "pwd" : password
+    } = event.target as HTMLFormElement
+
+    event.preventDefault()
+
+    const userDetails = {
+      email: email.value,
+      password: password.value
+    }
+
+    fetch("http://localhost:3100/login",{
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(userDetails)
+    })
+    .then(res=>{
+      if (res.ok) {
+        
+      }
+    })
+  }
+
   return (
     <header className="headerStyling">
       <div>logo</div>
@@ -15,11 +46,11 @@ export default function LandingPageHeader() {
       >
         Sign Up
       </div>
-      <form>
+      <form onSubmit={handlesubmit}>
         <label htmlFor="email">Log In with Email:</label>
         <input type="email" id="email" name="email" />
         <label htmlFor="pwd">Password:</label>
-        <input type="password" id="pwd" name="pwd" minLength="8" />
+        <input type="password" id="pwd" name="pwd" minLength={8} />
         <button className="logInButton">Submit</button>
       </form>
     </header>
