052a59b1ae233bdd1112d267f94e5e035f838f5c
brokensythe <ade_1.0@live.co.uk>
2021-09-06 15:17:24 +0100

    added track and listing data

--- backend/src/utils/mockData.ts
+++ backend/src/utils/mockData.ts
@@ -24,41 +24,55 @@ export const users = [
         "email": "ronnie@corbett.com"
     }
 ]
-export let listings =[
-    {
+export let tracks =[
+      {
         artistName:"Blondie",
         trackName:"Dreaming",
-        owner:"Joe Blogs",
         coverURL:"https://images.eil.com/large_image/BLONDIE_DREAMING-330467.jpg",
-        condition:"Good",
-        format:"7 Inch",
-        price:5.49
       },
       {
         artistName:"Alphonse Mouzon",
         trackName:"Don't Want To Lose This Feeling",
-        owner:"Greg Edwards",
         coverURL:"https://images-na.ssl-images-amazon.com/images/I/41kTf4lKDJL.jpg",
-        condition:"Good",
-        format:"12 Inch",
-        price:10.49
       },
       {
         artistName:"The Heptones",
         trackName:"I Hold The Handle",
-        owner:"Coxsone Dodd",
         coverURL:"HTTP://images.45cat.com/the-heptones-handle-dub-hep-hep-s.jpg",
-        condition:"Good",
-        format:"7 Inch",
-        price:15.49
       },
       {
         artistName:"Errol Dunkley",
         trackName:"Betcha By Golly Wow",
-        owner:"Sydney",
         coverURL:"https://img.discogs.com/7ofA7QaVs1K3gsmW_lpjZt9Jeyc=/fit-in/600x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-5331775-1401387220-3449.jpeg.jpg",
-        condition:"Good",
-        format:"7 Inch",
-        price:5.49
       }
+]
+export const listings = [
+  {
+    user: 1,
+    track: 1,
+    price: 5.49,
+    condition: "Good",
+    notes: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Velit sed ullamcorper morbi tincidunt ornare massa eget egestas purus."
+  },
+  {
+    user: 2,
+    track: 2,
+    price: 10.49,
+    condition: "Good",
+    notes: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Velit sed ullamcorper morbi tincidunt ornare massa eget egestas purus."
+  },
+  {
+    user: 3,
+    track: 3,
+    price: 15.49,
+    condition: "Good",
+    notes: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Velit sed ullamcorper morbi tincidunt ornare massa eget egestas purus."
+  },
+  {
+    user: 4,
+    track: 4,
+    price: 5.49,
+    condition: "Good",
+    notes: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Velit sed ullamcorper morbi tincidunt ornare massa eget egestas purus."
+  }
 ]
