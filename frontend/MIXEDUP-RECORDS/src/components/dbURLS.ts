8a8aced227c4b7ad55b31317e798a2305c790d4e
sydney.murray <sydney_i_murray@yahoo.co.uk>
2021-09-07 15:42:50 +0100

    Updates

--- frontend/MIXEDUP-RECORDS/src/components/dbURLS.ts
+++ frontend/MIXEDUP-RECORDS/src/components/dbURLS.ts
@@ -1,2 +1,2 @@
 export let LATEST_LISTINGS: string = "http://localhost:3100/latestlistings"
-export let REGISTER_NEW_USER: string = "http://localhost:3100/users"
+export let REGISTER_NEW_USER: string = "http://localhost:3100/sign-up"
